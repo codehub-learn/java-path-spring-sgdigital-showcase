0177548d815164be9efd5c99f1cbc5da87a1935b
Constantinos Giannacoulis <c.giannacoulis@codehub.gr>
2022-11-02 12:51:15 +0200

    Showcased the cases of query hints and how to run custom SQL queries and have the information returned mapped to custom DTOs.
    Signed-off-by: Constantinos Giannacoulis <c.giannacoulis@codehub.gr>

--- java-path-spring-app/src/main/java/gr/codelearn/spring/showcase/app/repository/OrderRepository.java
+++ java-path-spring-app/src/main/java/gr/codelearn/spring/showcase/app/repository/OrderRepository.java
@@ -2,22 +2,30 @@ package gr.codelearn.spring.showcase.app.repository;
 
 import gr.codelearn.spring.showcase.app.domain.Order;
 import gr.codelearn.spring.showcase.app.transfer.KeyValue;
+import gr.codelearn.spring.showcase.app.transfer.PurchasesCostPerCustomerCategoryDto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.QueryHints;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.QueryHint;
 import java.math.BigDecimal;
 import java.util.List;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
-	@Query("select new gr.codelearn.spring.showcase.app.transfer.KeyValue(" +
-			"concat(c.firstname, ' ', c.lastname), " +
-			"avg(o.cost)) " +
-			"from Order o " +
-			"join o.customer c group by c.firstname")
+	@Query("""
+			           select new gr.codelearn.spring.showcase.app.transfer.KeyValue(concat(c.firstname, ' ', c.lastname),
+					avg(o.cost))
+					from Order o
+					join o.customer c group by c.firstname
+			""")
 	List<KeyValue<String, BigDecimal>> findAverageOrderCostPerCustomer();
 
-	@Query("select o from Order o join fetch o.customer join fetch o.orderItems where o.id = :id")
+	@Query("select distinct o from Order o join fetch o.customer join fetch o.orderItems oi join fetch oi.product where o.id = :id")
+	@QueryHints(@QueryHint(name = org.hibernate.jpa.QueryHints.HINT_PASS_DISTINCT_THROUGH, value = "false"))
 	Order getLazy(Long id);
+
+	@Query(nativeQuery = true)
+	List<PurchasesCostPerCustomerCategoryDto> findTotalNumberOfOrdersAndCostOfPurchasesPerCustomerCategory();
 }
